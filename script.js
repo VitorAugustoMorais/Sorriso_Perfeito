8c925cf0177c61bc8739d8febac719b99f3a6843
Vitor Augusto <vitormoraisaugusto@gmail.com>
2024-07-06 13:32:42 -0300

    botão whatsapp criado

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const closeMenu = document.getElementById('closeMenu')
 const openMenu = document.getElementById('openMenu')
+const closeWhatsapp = document.getElementById('closeWhatsapp')
 
 closeMenu.addEventListener('click', menu)
 openMenu.addEventListener('click', menu)
+closeWhatsapp.addEventListener('click', closedWhatsapp)
 
-function menu (){
+function menu() {
     const menu = document.getElementById('menu')
 
-    if (openMenu.dataset.condition === 'on'){
+    if (openMenu.dataset.condition === 'on') {
         openMenu.dataset.condition = 'off'
         openMenu.style.display = 'none'
         closeMenu.style.display = 'inline'
@@ -18,4 +20,9 @@ function menu (){
         closeMenu.style.display = 'none'
         menu.style.display = 'none'
     }
+}
+
+function closedWhatsapp() {
+    const btnWhatsapp = document.getElementById('btnWhatsapp')
+    btnWhatsapp.style.display = 'none'
 }
